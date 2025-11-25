597946f935689a48dfd7638eec32efc296206ca8
reeshav <reeshav.kumar@mail.utoronto.ca>
2025-11-25 02:36:08 -0500

    Preflight Fix

--- backend/routes.js
+++ backend/routes.js
@@ -1,15 +1,13 @@
 import express from "express";
 import { loginController, profileController, registerController } from "./controllers/user.js";
 import { authToken } from "./middleware/auth.js";
+import cors from "cors";
 
 const router = express.Router();
-var cors = require('cors')
+router.options("*", cors());
 
-
-app.options('*', cors());
-
-router.post('/login', loginController);
-router.get('/user/me', authToken, profileController);
-router.post('/register', registerController);
+router.post("/login", loginController);
+router.get("/user/me", authToken, profileController);
+router.post("/register", registerController);
 
 export default router;
